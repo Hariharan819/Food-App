6ae77cdcee123f7b33e731618921efe698cfd190
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-06-14 21:50:40 +0530

    food app commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# AppLayout
+ # Header
+  -Logo
+  -Nav-Items
+    -Home
+    -About
+    -cart
+ # Body
+  -Search
+  -restaurantContainer
+    -restaurantCards
+ # Fotter 
+  -copyright
+  -address
+  -Links
+  -Address
+#
--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const SubHeading = () => (
-  <h3 className="Subheading">SubHeading Functional Component🚀</h3>
-);
+const Header = () => {
+  return (
+    <div className="head">
+      <div className="img-container">
+        <img
+          className="logo"
+          src="https://tse1.mm.bing.net/th?id=OIP.ZRB8nfYxIqe_AXkygaFEJQAAAA&pid=Api&P=0&h=180"
+        />
+      </div>
+      <div className="nav_items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Heading = () => (
-  <div className="SubRoot">
-    <h3>Functional Component🚀</h3>
-    <SubHeading />
-  </div>
-);
+const RestaurantCards=()=>{
+    return(
+        <div className="restaurant-cards" style={{backgroundColor:"grey"}}>
+            <h4>Aachii</h4>
+        </div>
+    );
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <div className="res-card">
+           <RestaurantCards/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body/>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);
+root.render(<AppLayout />);
