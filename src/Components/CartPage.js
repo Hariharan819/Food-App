54d1d7a460991a3a4f802baac146b38b2c28b875
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-06-29 15:43:39 +0530

    card clr function

--- src/Components/CartPage.js
+++ src/Components/CartPage.js
@@ -1,9 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import RestaurantmenuitemList from "./RestaurantmenuitemList";
+import { clearItem } from "../ReduxUtilis/cartSlice";
 const Cartpage = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  const clrcartfun = () => {
+    dispatch(clearItem());
+  };
   return (
     <div className="flex flex-col items-center">
+      <button
+        className="p-2 rounded-md bg-slate-500 my-2 text-white"
+        onClick={clrcartfun}
+      >
+        Clear-Cart
+      </button>
+
       <div className="mx-10 my-5 w-5/12 items-center">
         <RestaurantmenuitemList items={cartItems} />
       </div>
